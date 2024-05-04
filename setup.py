5da1eac6363fba3eaa80d3280ef8a523667d17b7
haaris ramzan <h.ramzan01@gmail.com>
2024-05-04 23:39:26 +0100

    chore: Update setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+# list dependencies from requirements.txt
+# list dependencies from file
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content]
+
+setup(
+    name='property_web_scraper',
+    description='A web scraper to scrape property data from a rightmove.co.uk search page.',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=requirements
+)
