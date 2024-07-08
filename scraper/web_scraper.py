3a16540c4127c9a741e710df23d9ec22426cee68
Haaris Ramzan <h.ramzan01@gmail.com>
2024-07-08 17:48:12 +0100

    updated to include lat and lon values

--- scraper/web_scraper.py
+++ scraper/web_scraper.py
@@ -1,63 +1,34 @@
 import requests
-import pandas as pd 
+import pandas as pd
 import time
 import random
 from bs4 import BeautifulSoup
+import json
+import os
 
-
-
-# Dictionary of London boroughs and their corresponding Rightmove IDs
-BOROUGHS = {
-    "City of London": "5E61224",
-    "Barking and Dagenham": "5E61400",
-    "Barnet": "5E93929",
-    "Bexley": "5E93932",
-    "Brent": "5E93935",
-    "Bromley": "5E93938",
-    "Camden": "5E93941",
-    "Croydon": "5E93944",
-    "Ealing": "5E93947",
-    "Enfield": "5E93950",
-    "Greenwich": "5E61226",
-    "Hackney": "5E93953",
-    "Hammersmith and Fulham": "5E61407",
-    "Haringey": "5E61227",
-    "Harrow": "5E93956",
-    "Havering": "5E61228",
-    "Hillingdon": "5E93959",
-    "Hounslow": "5E93962",
-    "Islington": "5E93965",
-    "Kensington and Chelsea": "5E61229",
-    "Kingston upon Thames": "5E93968",
-    "Lambeth": "5E93971",
-    "Lewisham": "5E61413",
-    "Merton": "5E61414",
-    "Newham": "5E61231",
-    "Redbridge": "5E61537",
-    "Richmond upon Thames": "5E61415",
-    "Southwark": "5E61518",
-    "Sutton": "5E93974",
-    "Tower Hamlets": "5E61417",
-    "Waltham Forest": "5E61232",
-    "Wandsworth": "5E93977",
-    "Westminster": "5E93980",
-}
-borough = BOROUGHS["Tower Hamlets"]
 
 # allows us to scrape the website without being blocked
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"
 }
 
-''' 
+# Load the boroughs json file
+boroughs = json.load(open('data/raw_data/boroughs.json'))
+
+'''
 00 PARSE RIGHTMOVE WEBSITE FOR PROPERTY DATA
 '''
-def scrape_rightmove(borough, num_pages):
+def scrape_rightmove():
     '''
     Function to scrape the Rightmove website for property data
     :param url: URL of the Rightmove website
     :return: List of prices, addresses, descriptions and links
     '''
+
+    num_pages = int(input("How many pages would you like to scrape? "))
+    user_location = str(input("Enter the borough you would like to scrape: "))
+    borough = boroughs[user_location]
+
     # Lists to store the desired data
     price_list = []
     address_list = []
@@ -74,7 +45,7 @@ def scrape_rightmove(borough, num_pages):
 
     for page in range(num_pages):
         print(f"Scraping page {page + 1}/{num_pages}...")
-        
+
         # the website changes if the you are on page 1 as compared to other pages
         if index == 0:
             url = f"https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=REGION%{borough}&sortType=6&propertyTypes=&includeSSTC=false&mustHave=&dontShow=&furnishTypes=&keywords="
@@ -136,7 +107,7 @@ def scrape_rightmove(borough, num_pages):
     '''
     03 CONVERT THE SCRAPED DATA INTO A PANDAS DATAFRAME & EXPORT TO CSV
     '''
-    
+
 
     #Create a dictionary to store the data
     data = {
@@ -147,15 +118,15 @@ def scrape_rightmove(borough, num_pages):
     }
 
     # Convert to dataframe and export csv
-    pd.DataFrame(data).to_csv('data/raw_data/rightmove_data.csv', index=False)
-    
+    pd.DataFrame(data).to_csv('data/processed_data/rightmove_data.csv', index=False)
+
     return data
 
 '''
 01 SCRAPE ADDITIONAL DATA FROM INDIVIDUAL PROPERTY PAGES
 '''
 def scrape_additional():
-    
+
     # Additional data to scrape
     property_type = []
     bedrooms = []
@@ -163,25 +134,28 @@ def scrape_additional():
     sq_ft = []
     sq_m = []
     tenure = []
+    latitude = []
+    longitude = []
+    postcode = []
 
 
     # import existing links from csv
-    df = pd.read_csv('data/raw_data/rightmove_data.csv')
+    df = pd.read_csv('data/processed_data/rightmove_data.csv')
     links = df['links']
 
     counter = 1
 
     for link in links:
         print(f"Scraping link {counter} of {len(links)}...")
-        
+
         url = link
         response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.text, 'html.parser')
 
         property_card = soup.find_all('dd', class_='_3ZGPwl2N1mHAJH3cbltyWn')
         info = [i.text.strip() for i in property_card]
-        
-        # skip property if data is missing
+
+        # skip property if data is missing ensuring lists same length
         if len(info) < 5:
             # append empty data to the lists
             property_type.append(info[0])
@@ -190,20 +164,49 @@ def scrape_additional():
             sq_ft.append(0)
             sq_m.append(0)
             tenure.append(0)
-            
+            latitude.append(0)
+            longitude.append(0)
+            postcode.append(0)
+
             # code to ensure that we do not overwhelm the website
             time.sleep(random.randint(1, 3))
             counter += 1
-            
+
             # continue to next iteration
             continue
-        
+
         # check if the data is missing
         if info[3] == 'Ask agent':
             info[3] = '0 sq ft 0 sq m'
         else:
             info[3] = info[3].replace(' sq m', '').replace('sq ft', '').replace(',', '').split()
 
+        ''' extract the latitude and longitude of the property '''
+        script_tag = None
+        for script in soup.find_all("script"):
+            if 'window.adInfo' in script.text:
+                script_tag = script
+                break
+
+        script_text = script_tag.string.strip()
+
+        # Split the script text and find the part containing 'window.PAGE_adInfo'
+        parts = script_text.split('window.adInfo = ')
+        converted = parts[1].strip('.propertyData.dfpAdInfo.targeting')
+
+        # Convert the JSON to a dictionary
+        dict = json.loads(converted)
+
+        # extract lat and lon from soup
+        lat = dict['propertyData']['location']['latitude']
+        lon = dict['propertyData']['location']['longitude']
+
+
+        # generate postcode from lat and lon
+        postcode_api = f'https://api.postcodes.io/postcodes?lon={lon}&lat={lat}'
+
+        response = requests.get(postcode_api).json()
+        pcode = response['result'][0]['postcode']
 
         # append the data to the lists
         property_type.append(info[0])
@@ -212,15 +215,21 @@ def scrape_additional():
         sq_ft.append(info[3][0])
         sq_m.append(info[3][1])
         tenure.append(info[4])
+        latitude.append(lat)
+        longitude.append(lon)
+        postcode.append(pcode)
 
         # code to ensure that we do not overwhelm the website
         time.sleep(random.randint(1, 3))
         counter += 1
 
+
     if len(property_type) == len(bedrooms) == len(bathrooms) == len(sq_ft) == len(sq_m) == len(tenure):
         print("Data scraped successfully!")
     else:
         print('length of list not matching!')
+
+
 
     # Create a dictionary to store the data
     data = {
@@ -229,29 +238,37 @@ def scrape_additional():
         "bathrooms": bathrooms,
         "sq_ft": sq_ft,
         "sq_m": sq_m,
-        "tenure": tenure
+        "tenure": tenure,
+        'latitude': latitude,
+        'longitude': longitude,
+        'postcode': postcode
     }
-    
+
     # Add to existing dataframe and export to csv
-    pd.DataFrame(data).to_csv('data/raw_data/rightmove_extra.csv', index=False)
-    
+    pd.DataFrame(data).to_csv('data/processed_data/rightmove_extra.csv', index=False)
+
     return data
-    
+
 '''
 02 COMBINE THE TWO DATAFRAMES INTO ONE
 '''
-def scrape_all(borough, num_pages):
+def scrape_all():
     # Call the two functions
-    basic_data = pd.DataFrame(scrape_rightmove(borough, num_pages))
+    basic_data = pd.DataFrame(scrape_rightmove())
     extra_data = pd.DataFrame(scrape_additional())
-    
+
     combined_data = pd.concat([basic_data, extra_data], axis=1)
-    
-    combined_data.to_csv('data/raw_data/rightmove_combined.csv', index=False)
+
+    combined_data.to_csv('data/processed_data/rightmove_combined.csv', index=False)
+
+    # delete the individual files
+    os.remove('data/processed_data/rightmove_data.csv')
+    os.remove('data/processed_data/rightmove_extra.csv')
+
 
 '''
 03 RUN THE FUNCTIONS
 '''
 # scrape_rightmove(borough=borough, num_pages=2)
 # scrape_additional()
-scrape_all(borough=borough, num_pages=2)
+scrape_all()
